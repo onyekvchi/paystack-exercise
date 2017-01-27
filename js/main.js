cd91975b71e8b448de86446720d9aa9745d6222b
Onyekachi <kcmailz@yahoo.com>
2017-01-27 23:07:09 +0100

    added input masks on card inpurd inputs

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
+//tab switching code
 $('.switcher__item--card').click(function(){
 	switchToThis($(this));
 	showCardForm();
+	clearAllFields();
 });
 
 $('.switcher__item--bank').click(function(){
 	switchToThis($(this));
 	showBankForm();
+	clearAllFields();
 });
-
-
 
 function switchToThis(element){
 	$('.switcher__item').removeClass('switcher__item--active');
@@ -23,4 +24,29 @@ function showBankForm(){
 function showCardForm(){
 	$('.payment-form--bank').addClass('hidden');
 	$('.payment-form--card').removeClass('hidden');
+	$('#card_number').focus();
 }
+
+function clearAllFields(){
+	$('.payment-form__field').val('');
+}
+
+
+//input masks code
+$(document).ready(function(){
+  $('#card_number')
+  	.mask('0000 0000 0000 0000', { 
+		onComplete: function(){
+			$('#valid_till').focus();
+		}
+	});
+
+  $('#valid_till')
+  	.mask('00/00', { 
+		onComplete: function(){
+			$('#cvv').focus();
+		}
+	});
+
+  $('#cvv').mask('000');
+});
